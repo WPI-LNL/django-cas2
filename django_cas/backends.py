b39ac89dfe06b66e878387d12895c4407e5f2489
gmorell <gmp@h4xmb.org>
2014-02-18 09:17:40 -0500

    Patch for reading return values, undid patch

--- django_cas/backends.py
+++ django_cas/backends.py
@@ -65,7 +65,6 @@ class CASBackend(ModelBackend):
                 return (None, None)
     
             username = response.getElementsByTagName('cas:user')[0].firstChild.nodeValue
-            x = f
             proxies = []
             if response.getElementsByTagName('cas:proxyGrantingTicket'):
                 proxies = [p.firstChild.nodeValue for p in response.getElementsByTagName('cas:proxies')]
@@ -85,7 +84,6 @@ class CASBackend(ModelBackend):
             logger.debug("Cas proxy authentication succeeded for %s with proxies %s", username, proxies)
             return (username, proxies)
         except Exception as e:
-            raise
             logger.error("Failed to verify CAS authentication", e)
             return (None, None)
         finally:
